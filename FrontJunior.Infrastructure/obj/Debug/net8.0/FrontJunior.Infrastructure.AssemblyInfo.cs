727d973c9b1dd6dc4fc62bb9f183ed7584dd00a8
Abu_Programmiy <abdukholiq23@gmail.com>
2024-07-27 17:30:17 +0500

    before evoulution

--- FrontJunior.Infrastructure/obj/Debug/net8.0/FrontJunior.Infrastructure.AssemblyInfo.cs
+++ FrontJunior.Infrastructure/obj/Debug/net8.0/FrontJunior.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontJunior.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f8b819f64b7b184d3d2e247a5f3f0b44503a572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+136cc88988ae3db6cf7928fa497fb0926bc074ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontJunior.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontJunior.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
