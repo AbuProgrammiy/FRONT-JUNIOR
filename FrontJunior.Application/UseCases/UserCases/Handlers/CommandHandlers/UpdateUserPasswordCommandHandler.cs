fa66178011cc1799b57b9a33ea382955603e144b
Abu_Programmiy <abdukholiq23@gmail.com>
2024-09-10 00:29:10 +0500

    UpdateUserPassword restored

--- FrontJunior.Application/UseCases/UserCases/Handlers/CommandHandlers/UpdateUserPasswordCommandHandler.cs
+++ FrontJunior.Application/UseCases/UserCases/Handlers/CommandHandlers/UpdateUserPasswordCommandHandler.cs
@@ -1,12 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FrontJunior.Application.Abstractions;
+using FrontJunior.Application.Services.PasswordServices;
+using FrontJunior.Application.UseCases.UserCases.Commands;
+using FrontJunior.Domain.Entities;
+using FrontJunior.Domain.Entities.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace FrontJunior.Application.UseCases.UserCases.Handlers.CommandHandlers
 {
-    internal class UpdateUserPasswordCommandHandler
+    public class UpdateUserPasswordCommandHandler : IRequestHandler<UpdateUserPasswordCommand, ResponseModel>
     {
+        private readonly IApplicationDbContext _applicationDbContext;
+        private readonly IPasswordService _passwordService;
+
+        public UpdateUserPasswordCommandHandler(IApplicationDbContext applicationDbContext, IPasswordService passwordService)
+        {
+            _applicationDbContext = applicationDbContext;
+            _passwordService = passwordService;
+        }
+
+        public async Task<ResponseModel> Handle(UpdateUserPasswordCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                User user = await _applicationDbContext.Users.FirstOrDefaultAsync(u => u.Id == request.Id);
+
+                if (user == null)
+                {
+                    return new ResponseModel
+                    {
+                        IsSuccess = false,
+                        StatusCode = 404,
+                        Response = "User not found!"
+                    };
+                }
+
+                bool isPasswordCorrect = _passwordService.CheckPassword(request.CurrentPassword, new PasswordModel
+                {
+                    PasswordHash = user.PasswordHash,
+                    PassworSalt = user.PassworSalt
+                });
+
+                if (isPasswordCorrect == false)
+                {
+                    return new ResponseModel
+                    {
+                        IsSuccess = false,
+                        StatusCode = 400,
+                        Response = "Current password is incorrect!"
+                    };
+                }
+
+                PasswordModel passwordModel = _passwordService.HashPassword(request.NewPassword);
+
+                user.PasswordHash = passwordModel.PasswordHash;
+                user.PassworSalt = passwordModel.PassworSalt;
+
+                await _applicationDbContext.SaveChangesAsync(cancellationToken);
+
+                return new ResponseModel
+                {
+                    IsSuccess = true,
+                    StatusCode = 200,
+                    Response = "Password successfuly updated!"
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseModel
+                {
+                    IsSuccess = false,
+                    StatusCode = 500,
+                    Response = "Something went wrong!"
+                };
+            }
+        }
     }
 }
